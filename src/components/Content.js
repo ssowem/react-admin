7df297ced1967ee4bdecccd85cf45b121f3424c5
soyeon <lsyeon0825@gmail.com>
2024-04-21 20:54:19 +0900

    메뉴바, 테일윈드CSS 수정

---
+++ src/components/Content.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Content() {
+  return (
+    <div className="bg-blue-100 grow">Content</div>
+  )
+}
+
+export default Content;
---
+++ src/components/Menubar.js
@@ -0,0 +1,131 @@
+import React, { useState } from 'react'
+
+function Menubar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(isOpen => !isOpen);
+  }
+
+  const defaultStyle = "flex justify-between w-full h-full px-1.5 py-1.5";
+  const activeDefaultStyle = isOpen ? "px-2 py-2 bg-white text-black rounded-sm" : "";
+
+  return (
+    <div className="bg-black h-screen w-64 px-3 py-3">
+      <div className="text-white text-2xl font-semibold">Pet-School Manager</div>
+      <div className="flex items-center gap-2 mt-1">
+        <img className="bg-white h-10 w-10 rounded-full" src="/images/pic3.png" alt="프로필사진" />
+        <div className="text-white flex-col">
+          <div className="text-lg">user-name01</div>
+          <span className="bg-slate-400 rounded-lg px-2 py-0.5">운영자</span>
+        </div>
+      </div>
+
+      <ul className="text-white flex flex-col gap-2 mt-10">
+        {/* 홈 메뉴 */}
+        <li>
+          <a className="flex justify-between w-full h-full px-1.5 py-1.5" href='/'>
+            <div className="flex">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+              </svg>
+
+              <div className="pl-1 text-sm font-semibold">홈</div>
+            </div>
+
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+            </svg>
+          </a>
+        </li>
+
+        {/* 학생관리 메뉴 */}
+        <li>
+          <button className={`${defaultStyle} ${activeDefaultStyle}`} onClick={toggleMenu}>
+            <div className="flex">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
+              </svg>
+
+              <div className="pl-1 text-sm font-semibold">학생관리</div>
+            </div>
+            <div>
+              {isOpen ? (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+              </svg>) : (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+              </svg>)}
+
+            </div>
+          </button>
+
+          {/* 서브메뉴 */}
+          <ul className={isOpen ? "block flex flex-col gap-1 mt-1" : "hidden"}>
+            <li>
+              <a className="flex justify-between w-full h-full px-1.5 py-1.5" href='/'>
+                <div className="pl-5">
+                  <div className="pl-1 text-sm font-semibold">학생 조회</div>
+                </div>
+                <div>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                  </svg>
+                </div>
+              </a>
+            </li>
+            <li>
+              <a className="flex justify-between w-full h-full px-1.5 py-1.5" href='/'>
+                <div className="pl-5">
+                  <div className="pl-1 text-sm font-semibold">출결 체크</div>
+                </div>
+                <div>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                  </svg>
+                </div>
+              </a>
+            </li>
+
+          </ul>
+        </li>
+
+        <li>
+          <a className="flex justify-between w-full h-full px-1.5 py-1.5" href='/'>
+            <div className="flex">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 1 1 0-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 0 1-1.44-4.282m3.102.069a18.03 18.03 0 0 1-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 0 1 8.835 2.535M10.34 6.66a23.847 23.847 0 0 0 8.835-2.535m0 0A23.74 23.74 0 0 0 18.795 3m.38 1.125a23.91 23.91 0 0 1 1.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 0 0 1.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 0 1 0 3.46" />
+              </svg>
+
+              <div className="pl-1 text-sm font-semibold">공지사항</div>
+            </div>
+            <div>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+              </svg>
+            </div>
+          </a>
+
+        </li>
+
+        <li>
+          <a className="flex justify-between w-full h-full px-1.5 py-1.5" href='/'>
+            <div className="flex">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75a4.5 4.5 0 0 1-4.884 4.484c-1.076-.091-2.264.071-2.95.904l-7.152 8.684a2.548 2.548 0 1 1-3.586-3.586l8.684-7.152c.833-.686.995-1.874.904-2.95a4.5 4.5 0 0 1 6.336-4.486l-3.276 3.276a3.004 3.004 0 0 0 2.25 2.25l3.276-3.276c.256.565.398 1.192.398 1.852Z" />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M4.867 19.125h.008v.008h-.008v-.008Z" />
+              </svg>
+
+              <div className="pl-1 text-sm font-semibold">메뉴01</div>
+            </div>
+            <div>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+              </svg>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div >
+  )
+}
+
+export default Menubar;
